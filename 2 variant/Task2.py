9c2c69d31851a9ae2bca13f51c77921734aa890a
slava <14escargot@gmail.com>
2022-06-01 23:12:05 +0300

    Задание 2 к ЕГЭ

---
+++ 2 variant/Task2.py
@@ -0,0 +1,10 @@
+u = [False, True]
+i = [False, True]
+o = [False, True]
+p = [False, True]
+for x in u:
+    for y in i:
+        for z in o:
+            for w in p:
+                if not (not w or (x or not z) and (not x or not y or z) and not w or (x or not z) and (not x or not y or z)):
+                    print(int(x), int(y), int(z), int(w))
